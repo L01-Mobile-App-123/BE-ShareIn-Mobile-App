99185aef7edd41ebe41cf256155e9eef6ef67b16
QuanTran <tranminhquan.411@gmail.com>
2025-11-24 19:52:07 +0700

    memory config

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document, swaggerOptions);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   
   // console.log(`🚀 Server is running on: http://localhost:${port}`);
   // console.log(`📚 Swagger docs available at: http://localhost:${port}/api/docs`);
